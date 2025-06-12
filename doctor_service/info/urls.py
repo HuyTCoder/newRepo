603302d219895629e3f69ea9d32c82cc450762fe
mcuong1011 <cuong2003.ttd@gmail.com>
2025-06-13 00:19:54 +0700

    cimmut

--- doctor_service/info/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import *
-urlpatterns = [
-    path('', create_doctor, name='create-doctor-info'),
-    path('doctors/', list_doctors, name='list-doctors'),
-    path('doctors/profile/<int:user_id>', get_doctor_profile, name='get_doctor_profile'),
-    path('doctors/update/<int:user_id>', update_doctor_profile, name='update_doctor_profile'),
-    path('doctors/<int:doctor_id>', get_doctor, name='get_doctor'),
-]
--- patient_service/core/utils/__init__.py
+++
@@ -1,1 +0,0 @@
-# Utilities package for patient service
